84b477e86d9ee5b14a9ebc87dddab13f393ba17c
Edgar Xolop <edgar@kettle.io>
2018-05-28 11:15:56 -0600

    refactoring views

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Receipt from '@/components/receipt/Receipt'
 import GenReceipt from '@/components/receipt/GenReceipt'
 
@@ -8,11 +7,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/print-receipt',
       name: 'Receipt',
