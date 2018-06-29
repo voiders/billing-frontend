592d9f8e38296b2f2a23af1dee2214a209d2a29c
Edgar Xolop <edgarxoix@gmail.com>
2018-06-28 21:21:44 -0600

    removing vuefire

--- src/main.js
+++ src/main.js
@@ -4,9 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import bulma from 'bulma/css/bulma.css'
-import VueFire from 'vuefire'
 
-Vue.use(VueFire)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
