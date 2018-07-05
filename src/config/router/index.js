5c497d46bee85c5abe23d0489ce666ade1e0fde6
Erick Damian <erickdlp95@gmail.com>
2018-07-04 22:10:00 -0600

    add product

--- src/config/router/index.js
+++ src/config/router/index.js
@@ -2,8 +2,11 @@ import VueRouter from 'vue-router'
 import Home from '@/components/Home'
 import SignIn from '@/components/account/signin'
 import SignUp from '@/components/account/signup'
+//import ListProduct from '@/components/items/products/List'
+import AddProduct from '@/components/items/products/Add'
 
 export default new VueRouter({
+  mode: 'history',
   routes: [ 
     {
       path: '/',
@@ -24,6 +27,11 @@ export default new VueRouter({
       path: '*',
       name: 'default',
       component: Home
+    },
+    {
+      path: '/products',
+      name: 'Products',
+      component: AddProduct
     }
   ]
 })
